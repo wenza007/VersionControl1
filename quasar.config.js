c3a6d1cf9c19413d104db12e347f9489de9209bb
wenza007 <overkung4399@gmail.com>
2025-09-17 11:49:50 +0700

    edit quasar.config.js and deploy.yml

--- quasar.config.js
+++ quasar.config.js
@@ -36,7 +36,7 @@ export default defineConfig((ctx) => {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-file#build
     build: {
-      // publicPath: '/',
+      publicPath: '/VersionControl1/',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
 
       // webpackTranspile: false,
